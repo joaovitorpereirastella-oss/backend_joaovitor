cfa5bf1cffcaa8ecaa8ab1b53a5c5dfdc86db4cc
João Vítor <joaovitorpereirastella@gmail.com>
2026-06-19 07:27:47 -0300

    Update ex07_1.js

--- lista01/ex07_1.js
+++ lista01/ex07_1.js
@@ -1,12 +1,9 @@
 let soma = 0;
 
-// Laço para percorrer os números de 1 a 50
 for (let i = 1; i <= 50; i++) {
-  // Verifica se o número atual é par
   if (i % 2 === 0) {
-    soma += i; // Adiciona o número par ao total acumulado
+    soma += i;
   }
 }
 
-// Exibe o resultado final
 console.log(`A soma de todos os números pares de 1 a 50 é: ${soma}`);
